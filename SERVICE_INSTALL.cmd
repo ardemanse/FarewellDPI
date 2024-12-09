f3dd9eb20e3af562dd1fedc5a5c65a168a8811de
ardemanse <187142255+ardemanse@users.noreply.github.com>
2024-12-09 11:43:26 +0300

    added new fakes, improved auto strategy

--- SERVICE_INSTALL.cmd
+++ SERVICE_INSTALL.cmd
@@ -5,13 +5,13 @@ del /F /Q logfile.log
 :: -=-=-=-=-=-=-=-=-=-=-= Edit strategies here -=-=-=-=-=-=-=-=-=-=-= ::
 set DISQ50000=--dpi-desync=fake --dpi-desync-fake-quic="%~dp0fake\quic_initial_vk_com.bin" --dpi-desync-cutoff=d2 --dpi-desync-any-protocol --dpi-desync-repeats=3
 
-set AUTO443=--dpi-desync=fake,fakeddisorder --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_dzen_ru.bin" --dpi-desync-fakedsplit-pattern="%~dp0fake\tls_clienthello_dzen_ru.bin" --dpi-desync-split-seqovl=1 --dpi-desync-split-pos=sniext+1 --dpi-desync-fooling=md5sig,badseq --dpi-desync-autottl=1 --dpi-desync-repeats=3
+set AUTO443=--dpi-desync=fake,multidisorder --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_stepik_org.bin" --dpi-desync-split-seqovl=1 --dpi-desync-split-pos=1,midsld-4,sniext,endhost-6 --dpi-desync-fooling=md5sig,badseq --dpi-desync-autottl=1 --dpi-desync-repeats=3
 
-set YT443=--dpi-desync=fake,fakedsplit --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_www_google_com.bin" --dpi-desync-fakedsplit-pattern="%~dp0fake\tls_clienthello_www_google_com.bin" --dpi-desync-split-seqovl=1 --dpi-desync-split-pos=sniext --dpi-desync-fooling=md5sig --dpi-desync-autottl=1 --dpi-desync-repeats=6
+set YT443=--dpi-desync=fake,fakedsplit --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_mail_google_com.bin" --dpi-desync-fakedsplit-pattern="%~dp0fake\tls_clienthello_www_google_com.bin" --dpi-desync-split-seqovl=1 --dpi-desync-split-pos=sniext --dpi-desync-fooling=md5sig --dpi-desync-autottl=1 --dpi-desync-repeats=6
 
 set YTDISQ443=--dpi-desync=fake,udplen --dpi-desync-fake-quic="%~dp0fake\quic_initial_www_google_com.bin" --dpi-desync-udplen-increment=10 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-cutoff=n2 --dpi-desync-repeats=6
 
-set BLACKLIST443=--dpi-desync=fake,multidisorder --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_dzen_ru.bin" --dpi-desync-split-pos=1,midsld+4,sniext-2,endhost-7 --dpi-desync-fooling=md5sig,datanoack --dpi-desync-autottl=1 --dpi-desync-repeats=3
+set BLACKLIST443=--dpi-desync=fake,multidisorder --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_yandex_net.bin" --dpi-desync-split-pos=1,midsld+4,sniext-2,endhost-7 --dpi-desync-fooling=md5sig,datanoack --dpi-desync-autottl=1 --dpi-desync-repeats=3
 
 set BLACKLIST80=--dpi-desync=fake,fakedsplit --dpi-desync-split-pos=method+3 --dpi-desync-fooling=md5sig,datanoack
 :: -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ::
