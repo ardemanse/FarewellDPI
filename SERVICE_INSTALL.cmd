aead853a78c1e47f87f9bde53d3ab72532c6b1eb
ardemanse <187142255+ardemanse@users.noreply.github.com>
2025-03-18 09:51:41 +0300

    update bin/strategies

--- SERVICE_INSTALL.cmd
+++ SERVICE_INSTALL.cmd
@@ -3,17 +3,17 @@ pushd "%~dp0"
 del /F /Q logfile.log
 
 :: -=-=-=-=-=-=-=-=-=-=-= Edit strategies here -=-=-=-=-=-=-=-=-=-=-= ::
-set BLACKLIST80=--dpi-desync=fake,multidisorder --dpi-desync-split-seqovl=5 --dpi-desync-split-pos=7,method+2,host+4,-32 --dpi-desync-fooling=md5sig,badseq --dpi-desync-repeats=4
+set BLACKLIST80=--dpi-desync=fake,multidisorder --dpi-desync-split-seqovl=5 --dpi-desync-split-pos=7,method+2,host+4,-32 --dpi-desync-fooling=md5sig,badseq --dpi-desync-repeats=3
 
 set BLACKLIST443=--dpi-desync=fake,multisplit --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_vk_com.bin" --dpi-desync-split-seqovl=517 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls_clienthello_mail_ru.bin" --dpi-desync-split-pos=sld-5,sld,midsld-1,midsld+1,endsld,sniext,sniext+16,host+2,host+7,-52,-42 --dpi-desync-fooling=md5sig,badseq --dpi-desync-autottl=1 --dpi-desync-repeats=6
 
 set AUTO443=--dpi-desync=fake,multisplit --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_mail_ru.bin" --dpi-desync-split-seqovl=398 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls_clienthello_vk_com.bin" --dpi-desync-split-pos=sld-2,midsld,endsld+1,sniext+7,host+1,endhost --dpi-desync-fooling=md5sig,badseq --dpi-desync-autottl=1 --dpi-desync-repeats=3
 
-set YT443=--dpi-desync=fake,fakedsplit --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_mail_google_com.bin" --dpi-desync-fakedsplit-pattern="%~dp0fake\tls_clienthello_www_google_com.bin" --dpi-desync-split-seqovl=404 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls_clienthello_www_google_com.bin" --dpi-desync-split-pos=midsld-1 --dpi-desync-fooling=md5sig,datanoack --dpi-desync-autottl=1 --dpi-desync-repeats=6
+set YT443=--dpi-desync=fake,fakedsplit --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_mail_google_com.bin" --dpi-desync-fakedsplit-pattern="%~dp0fake\tls_clienthello_www_google_com.bin" --dpi-desync-split-seqovl=404 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls_clienthello_www_google_com.bin" --dpi-desync-split-pos=midsld-1 --dpi-desync-fooling=md5sig,datanoack --dpi-desync-autottl=1 --dpi-desync-repeats=8
 
-set QUIC443=--dpi-desync=fake,udplen --dpi-desync-fake-quic="%~dp0fake\quic_initial_vk_com.bin" --dpi-desync-udplen-increment=8 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-autottl=1 --dpi-desync-repeats=12
+set QUIC443=--dpi-desync=fake,udplen --dpi-desync-fake-quic="%~dp0fake\quic_initial_vk_com.bin" --dpi-desync-udplen-increment=8 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-autottl=1 --dpi-desync-repeats=8
 
-set DISQ50000=--dpi-desync=fake,udplen --dpi-desync-fake-quic="%~dp0fake\quic_initial_vk_com.bin" --dpi-desync-udplen-increment=24 --dpi-desync-cutoff=d2 --dpi-desync-any-protocol --dpi-desync-repeats=9
+set DISQ50000=--dpi-desync=fake,udplen --dpi-desync-fake-quic="%~dp0fake\quic_initial_vk_com.bin" --dpi-desync-udplen-increment=24 --dpi-desync-cutoff=d2 --dpi-desync-any-protocol --dpi-desync-repeats=4
 :: -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ::
 
 set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-50099 --filter-tcp=80 --hostlist="%~dp0lists\blacklist.txt" --hostlist-exclude="%~dp0lists\exclude.txt" --hostlist="%~dp0lists\customhostlist.txt" %BLACKLIST80% --new --filter-tcp=443 --hostlist="%~dp0lists\blacklist.txt" --hostlist-exclude="%~dp0lists\exclude.txt" --hostlist="%~dp0lists\customhostlist.txt" --hostlist="%~dp0lists\discord.txt" %BLACKLIST443% --new --filter-tcp=443 --hostlist-auto="%~dp0lists\autohostlist.txt" --hostlist-auto-retrans-threshold=2 --hostlist-auto-fail-threshold=3 --hostlist-auto-fail-time=30 --hostlist-exclude="%~dp0lists\exclude.txt" %AUTO443% --new --filter-tcp=443 --hostlist="%~dp0lists\youtube.txt" %YT443% --new --filter-udp=443 %QUIC443% --new --filter-udp=50000-50099 %DISQ50000%
