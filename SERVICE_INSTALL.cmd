d0ef8bb68729ce33bc549061ceed4385cbbd9604
ardemanse <187142255+ardemanse@users.noreply.github.com>
2025-04-25 09:42:13 +0300

    Update SERVICE_INSTALL.cmd

--- SERVICE_INSTALL.cmd
+++ SERVICE_INSTALL.cmd
@@ -3,9 +3,9 @@ pushd "%~dp0"
 del /F /Q logfile.log
 
 :: -=-=-=-=-=-=-=-=-=-=-= Edit strategies here -=-=-=-=-=-=-=-=-=-=-= ::
-set BLACKLIST80=--dpi-desync=multisplit --dpi-desync-repeats=3 --dpi-desync-split-pos=7,method+2,host+4,-32
+set BLACKLIST80=--dpi-desync=fake,multisplit --dpi-desync-repeats=3 --dpi-desync-fake-http="%~dp0fake\http_iana_org.bin" --dpi-desync-split-pos=7,method+2,host+4,-32 --dpi-desync-fooling=badseq
 
-set BLACKLIST443=--dpi-desync=multisplit --dpi-desync-split-seqovl=496 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls_clienthello_ads_vk_com_tls12.bin" --dpi-desync-split-pos=sld-20,sld-14,sld-9,sld-5,sld-2,sld,midsld-1,midsld,midsld+2,endsld,endsld+2,endsld+4,endsld+8,endsld+16,endsld+32
+set BLACKLIST443=--dpi-desync=fake,multisplit --dpi-desync-repeats=3 --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_www_google_com_tls12.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_ads_vk_com_tls12.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-split-seqovl=494 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls_clienthello_ads_vk_com_tls12.bin" --dpi-desync-split-pos=sniext-42,sniext-36,sniext-24,sniext-18,sniext+3,sld-5,sld-2,sld,midsld-2,midsld,midsld+2,endsld+1,endsld+2,endsld+7,endsld+14,endsld+21,endsld+33,endsld+42,endsld+52,endsld+69 --dpi-desync-fooling=datanoack
 
 set QUIC443=--dpi-desync=fake,ipfrag2 --dpi-desync-repeats=8 --dpi-desync-fake-quic="%~dp0fake\quic_initial_vk_com.bin" --dpi-desync-ipfrag-pos-udp=32
 
