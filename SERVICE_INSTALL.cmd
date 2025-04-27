3633bca11bf55c69d370a7557f47022de6768fdf
ardemanse <187142255+ardemanse@users.noreply.github.com>
2025-04-27 15:50:20 +0300

    Update SERVICE_INSTALL.cmd

--- SERVICE_INSTALL.cmd
+++ SERVICE_INSTALL.cmd
@@ -3,16 +3,16 @@ pushd "%~dp0"
 del /F /Q logfile.log
 
 :: -=-=-=-=-=-=-=-=-=-=-= Edit strategies here -=-=-=-=-=-=-=-=-=-=-= ::
-set BLACKLIST80=--dpi-desync=fake,multisplit --dpi-desync-repeats=3 --dpi-desync-fake-http="%~dp0fake\http_iana_org.bin" --dpi-desync-split-pos=7,method+2,host+4,-32 --dpi-desync-fooling=badseq
+set HTTP80=--dpi-desync=fake,multisplit --dpi-desync-repeats=12 --dpi-desync-fake-http="%~dp0fake\http_iana_org.bin" --dpi-desync-split-pos=9,method+1,host,host+2,host+5,host+9,-11 --dpi-desync-fooling=badseq
 
-set BLACKLIST443=--dpi-desync=fake,multisplit --dpi-desync-repeats=3 --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_www_google_com_tls12.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_ads_vk_com_tls12.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-split-seqovl=494 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls_clienthello_ads_vk_com_tls12.bin" --dpi-desync-split-pos=sniext-42,sniext-36,sniext-24,sniext-18,sniext+3,sld-5,sld-2,sld,midsld-2,midsld,midsld+2,endsld+1,endsld+2,endsld+7,endsld+14,endsld+21,endsld+33,endsld+42,endsld+52,endsld+69 --dpi-desync-fooling=datanoack
+set TLS443=--dpi-desync=fake,multisplit --dpi-desync-repeats=3 --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_www_google_com_tls12.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-fake-tls="%~dp0fake\tls_clienthello_ads_vk_com_tls12.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-split-seqovl=488 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls_clienthello_ads_vk_com_tls12.bin" --dpi-desync-split-pos=sld-20,sld-14,sld-9,sld-5,sld-2,sld,midsld-1,midsld,midsld+2,endsld,endsld+2,endsld+4,endsld+8,endsld+16,endsld+32 --dpi-desync-fooling=datanoack
 
-set QUIC443=--dpi-desync=fake,ipfrag2 --dpi-desync-repeats=8 --dpi-desync-fake-quic="%~dp0fake\quic_initial_vk_com.bin" --dpi-desync-ipfrag-pos-udp=32
+set QUIC443=--dpi-desync=fake,ipfrag2 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0fake\quic_initial_vk_com.bin" --dpi-desync-ipfrag-pos-udp=32
 
-set DISQ50000=--dpi-desync=fake,udplen --dpi-desync-repeats=4 --dpi-desync-fake-unknown-udp="%~dp0fake\quic_short_header.bin" --dpi-desync-udplen-increment=8 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-any-protocol --dpi-desync-cutoff=d2
+set DISCORDSTUN50000=--dpi-desync=fake,udplen --dpi-desync-repeats=3 --dpi-desync-fake-discord="%~dp0fake\quic_short_header.bin" --dpi-desync-fake-stun="%~dp0fake\quic_short_header.bin" --dpi-desync-udplen-increment=0xDEADBEEF
 :: -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ::
 
-set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-50099 --filter-tcp=80 --hostlist="%~dp0lists\blacklist.txt" --hostlist="%~dp0lists\customhostlist.txt" --hostlist-exclude="%~dp0lists\exclude.txt" %BLACKLIST80% --new --filter-tcp=443 --hostlist="%~dp0lists\blacklist.txt" --hostlist="%~dp0lists\customhostlist.txt" --hostlist="%~dp0lists\discord.txt" --hostlist="%~dp0lists\youtube.txt" %BLACKLIST443% --new --filter-tcp=443 --hostlist-auto="%~dp0lists\autohostlist.txt" --hostlist-exclude="%~dp0lists\exclude.txt" %BLACKLIST443% --new --filter-udp=443 %QUIC443% --new --filter-udp=50000-50099 %DISQ50000%
+set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-50099 --filter-tcp=80 --hostlist="%~dp0lists\blacklist.txt" --hostlist="%~dp0lists\customhostlist.txt" --hostlist="%~dp0lists\discord.txt" --hostlist="%~dp0lists\youtube.txt" %HTTP80% --new --filter-tcp=443 --hostlist="%~dp0lists\blacklist.txt" --hostlist="%~dp0lists\customhostlist.txt" --hostlist="%~dp0lists\discord.txt" --hostlist="%~dp0lists\youtube.txt" %TLS443% --new --filter-tcp=443 --hostlist-auto="%~dp0lists\autohostlist.txt" --hostlist-exclude="%~dp0lists\exclude.txt" %TLS443% --new --filter-udp=443 %QUIC443% --new --filter-udp=50000-50099 --filter-l7=discord,stun %DISCORDSTUN50000%
 
 set ARGS=%ARGS:"=\"%
 
