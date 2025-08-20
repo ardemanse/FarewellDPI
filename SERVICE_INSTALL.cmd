9fe89e6a10ae2a4aa063cb885f0d89239bf15805
Septonious <187142255+ardemanse@users.noreply.github.com>
2025-08-20 14:59:24 +0300

    update strats and lists

--- SERVICE_INSTALL.cmd
+++ SERVICE_INSTALL.cmd
@@ -3,14 +3,16 @@ pushd "%~dp0"
 del /F /Q logfile.log
 
 :: -=-=-=-=-=-=-=-=-=-=-= Edit strategies here -=-=-=-=-=-=-=-=-=-=-= ::
-set TLS443=--dpi-desync=fake,multisplit --dpi-desync-repeats=3 --dpi-desync-fake-tls="%~dp0fake\tls13_clienthello_vk_com.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-fake-tls="%~dp0fake\tls13_clienthello_api_vk_com.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-split-seqovl=476 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls13_clienthello_api_vk_com.bin" --dpi-desync-split-pos=62,sniext,host+2,sld-3,sld+2,midsld,endsld-1,endsld+2,-17 --dpi-desync-fooling=datanoack,badseq --dpi-desync-badseq-increment=0x80000000
+set MAIN_TLS443=--dpi-desync=fake,multisplit --dpi-desync-repeats=2 --dpi-desync-fake-tls="%~dp0fake\tls13_clienthello_vk_com.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-fake-tls="%~dp0fake\tls13_clienthello_api_vk_com.bin" --dpi-desync-fake-tls-mod=rnd,dupsid,padencap --dpi-desync-split-seqovl=487 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls13_clienthello_api_vk_com.bin" --dpi-desync-split-pos=sniext,sniext+2,host+1,sld-2,midsld-3,midsld,endsld-2,endsld+2 --dpi-desync-fooling=datanoack,badseq --dpi-desync-badseq-increment=0x80000000
+
+set YT_TLS443=--dpi-desync=fake,multisplit --dpi-desync-repeats=2 --dpi-desync-fake-tls="%~dp0fake\tls13_clienthello_drive_google_com.bin" --dpi-desync-fake-tls-mod=rnd,dupsid --dpi-desync-fake-tls="%~dp0fake\tls13_clienthello_mail_google_com.bin" --dpi-desync-fake-tls-mod=rnd,dupsid,padencap --dpi-desync-split-seqovl=493 --dpi-desync-split-seqovl-pattern="%~dp0fake\tls13_clienthello_mail_google_com.bin" --dpi-desync-split-pos=sniext,host+1,host+5,midsld-3,midsld,midsld+2,endsld-3,endsld+2 --dpi-desync-fooling=datanoack,badseq --dpi-desync-badseq-increment=0x80000000
 
 set QUIC443=--dpi-desync=fake,ipfrag2 --dpi-desync-repeats=5 --dpi-desync-fake-quic="%~dp0fake\quic_initial_vk_com.bin" --dpi-desync-ipfrag-pos-udp=32
 
-set DISCORDSTUN50000=--dpi-desync=fake --dpi-desync-repeats=2 --dpi-desync-fake-discord="%~dp0fake\quic_short_header.bin" --dpi-desync-fake-stun="%~dp0fake\quic_short_header.bin"
+set DISCORDSTUN50000=--dpi-desync=fake --dpi-desync-repeats=3 --dpi-desync-fake-discord="%~dp0fake\quic_short_header.bin" --dpi-desync-fake-stun="%~dp0fake\quic_short_header.bin" --dpi-desync-udplen-increment=8 --dpi-desync-udplen-pattern=0xDEADBEEF
 :: -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ::
 
-set ARGS=--wf-tcp=443 --wf-udp=443,50000-50099 --filter-tcp=443 --hostlist="%~dp0lists\blacklist.txt" --hostlist="%~dp0lists\customhostlist.txt" %TLS443% --new --filter-tcp=443 --hostlist="%~dp0lists\youtube.txt" %TLS443% --new --filter-tcp=443 --hostlist-auto="%~dp0lists\autohostlist.txt" --hostlist-exclude="%~dp0lists\exclude.txt" %TLS443% --new --filter-udp=443 %QUIC443% --new --filter-udp=50000-50099 --filter-l7=discord,stun %DISCORDSTUN50000%
+set ARGS=--wf-tcp=443 --wf-udp=443,50000-50099 --filter-tcp=443 --hostlist="%~dp0lists\zapret-hosts-main.txt" --hostlist="%~dp0lists\zapret-hosts-user.txt" %MAIN_TLS443% --new --filter-tcp=443 --hostlist="%~dp0lists\zapret-hosts-google.txt" %YT_TLS443% --new --filter-udp=443 %QUIC443% --new --filter-udp=50000-50099 --filter-l7=discord,stun %DISCORDSTUN50000%
 
 set ARGS=%ARGS:"=\"%
 
